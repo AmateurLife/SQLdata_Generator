b13ff6d56fc4a1545eecb4ac28a5bafbc66f3c67
amateurlife <amateurlife@163.com>
2026-05-18 17:14:26 +0800

    添加项目文件。

---
+++ SQLdata_Generator/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using SQLdata_Generator.Views;
+
+namespace SQLdata_Generator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ SQLdata_Generator/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SQLdata_Generator.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
